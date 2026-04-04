a23cab9d99aa8110ad7109731f729da128705460
Anjali <anjali999kushwaha@gmail.com>
2026-04-05 05:01:10 +0530

    updated video_stats to extract video data

--- video_stats.py
+++ video_stats.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from itertools import islice
 
 import os
 from dotenv import load_dotenv
@@ -79,8 +80,74 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
+
+
+
+def extract_video_data(video_ids):
+
+    extracted_data = []
+
+    def batch_list(video_ids):
+        # Create an iterator
+        it = iter(video_ids)
+
+        # Use islice to generate chunks of size defined in maxResult
+        batches = []
+        for _ in range(0, len(video_ids), maxResults):
+            batches.append(list(islice(it, maxResults)))
+
+        return batches
+    
+    try:
+
+        for batch in batch_list(video_ids):
+            
+            video_ids_batch = ",".join(batch)
+
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={video_ids_batch}&key={API_KEY}'
+
+            response = requests.get(url)
+            response.raise_for_status()
+                
+            data = response.json()
+            # print(json.dumps(data,indent=4))
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+            
+                video_data = {
+                    'video_id' : video_id,
+                    'title' : snippet['title'],
+                    'publishedAt' : snippet['publishedAt'],
+                    'duration' : contentDetails['duration'],
+                    'viewCount' : statistics.get('viewCount', None),            #using get to handle cases where the author hides views, likes, or comments and the API returns inconsistent data.
+                    'likeCount' : statistics.get('likeCount', None),
+                    'commentCount' : statistics.get('commentCount', None),
+                }
+
+                extracted_data.append(video_data)
+        return extracted_data 
+            
+
+            # data['items'][0]['snippet']['title'],
+            # data['items'][0]['snippet']['publishedAt'],
+            # data['items'][0]['contentDetails']['duration'],
+            # data['items'][0]['statistics']['viewCount'],
+            # data['items'][0]['statistics']['likeCount'],
+            # data['items'][0]['statistics']['commentCount']
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+
 if __name__ == "__main__":
 
     playlistId = get_playlist_id()
-    print(get_video_ids(playlistId))
+    video_ids = get_video_ids(playlistId)
+
+    extracted_data = extract_video_data(video_ids)
+    print(len(extracted_data))      # should be equal to no. of videos posted by the channel
     
